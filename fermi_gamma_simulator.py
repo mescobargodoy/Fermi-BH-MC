61a1324be8600983549dec79e5254de3fff9ae60
mescobargodoy <mescob11@ucsc.edu>
2024-02-28 14:33:32 -0800

    Added new method for calculation of errors.

---
+++ fermi_gamma_simulator.py
@@ -0,0 +1,13 @@
+from simple_mc_funcs import*
+
+
+class PairProductionCS():
+    """
+    Class for generation of photons 
+    following a simple power law 
+    distribution. 
+    """
+
+
+    def __init__(self):
+        # TODO
